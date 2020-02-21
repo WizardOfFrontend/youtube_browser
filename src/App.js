48e774ed2a5b14d7fdab57b8b7b23c0c28840a04
wizardofqa <wizardofqa@outlook.com>
2020-02-21 07:47:00 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import Youtube from './apis/Youtube';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+import './App.css';
 
 class App extends Component {
-  state = { videos: [] };
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
 
+  componentDidMount() {
+    this.onSearchTermSubmit('Frozen 2')
+  }
   onSearchTermSubmit = async searchTerm => {
     const response = await Youtube.get('/search', {
       params: {
         q: searchTerm
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   }
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video })
+
+  }
+
   render() {
     return (
       <div className="ui container">
         <h1 className="ui center aligned icon header">
           <i className="youtube red icon"></i>
-          Youtube Browser
+          Toby's Youtube Browser
         </h1>
         <SearchBar onFormSubmit={this.onSearchTermSubmit} />
-        I have {this.state.videos.length} videos.
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
